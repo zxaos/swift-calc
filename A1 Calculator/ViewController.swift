31b029662608fa5e2b8c8267573bfdca985a7cb1
Matt Bond <matt@codefire.io>
2015-02-01 17:10:53 -0500

    Initial Commit

---
+++ A1 Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A1 Calculator
+//
+//  Created by Matt Bond on 2015-02-01.
+//  Copyright (c) 2015 Codefire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
