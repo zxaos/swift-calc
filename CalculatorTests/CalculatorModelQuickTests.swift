eaf01d4eab6021d10727fd4d50a2bc097fb673f4
Matt Bond <matt@codefire.io>
2015-06-14 00:05:21 -0400

    move some tests over to Quick

--- CalculatorTests/CalculatorModelQuickTests.swift
+++ CalculatorTests/CalculatorModelQuickTests.swift
@@ -12,14 +12,50 @@ import Calculator
 
 class CalculatorModelQuickTests: QuickSpec {
     override func spec() {
-        var c : CalculatorModel
+        var c : CalculatorModel!
         
         beforeEach {
             c = CalculatorModel();
         }
         
-        describe("the 'Calculator Brain' Model"){
+        describe("the Calculator Model"){
             
+            it("applies variables when calculating"){
+                //given r = 6 - x * 32 ;  x = -9 -> y = 294
+                c.pushOperand(6)
+                c.pushOperand("x")
+                c.pushOperand(32)
+                c.performOperation("×")
+                c.performOperation("−")
+                
+                c.variableValue["x"] = -9
+                
+                expect(c.evaluate()).to(equal(294))
+            }
+            
+            xit("clears all data upon request"){}
+            
+            xit("applies addition and multiplication with correct order of operations"){
+                // 2 + 3 * 4 = 14
+                // (2 + 3) * 4 = 20
+                c.pushOperand(2)
+                c.pushOperand(3)
+                c.pushOperand(4)
+                c.performOperation("×")
+                c.performOperation("+")
+                expect(c.evaluate()).to(equal(14))
+                // 1 * 2 + 3 = 5
+                c.resetOperands()
+                c.pushOperand(1)
+                c.pushOperand(2)
+                c.pushOperand(3)
+                c.performOperation("+")
+                c.performOperation("×")
+//                XCTAssertNotNil(result)
+//                if result != nil {
+//                    XCTAssertEqual(result!, 5.0)
+//                }
+            }
         }
     }
 }
