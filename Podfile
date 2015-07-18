b5c5a438b08cb17186a8d7598c385a645b31286c
Matt Bond <matt@codefire.io>
2015-07-18 14:20:00 -0400

    get really irritated with Cocoapods and then switch test libraries to submodules

--- Podfile
+++
@@ -1,7 +0,0 @@
-# Podfile
-
-target 'Calculator' do
-  use_frameworks!
-  pod 'Quick', '~> 0.3.1'
-  pod 'Nimble'
-end
--- Pods/Target Support Files/Pods-Calculator-Quick/Pods-Calculator-Quick-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Calculator_Quick : NSObject
-@end
-@implementation PodsDummy_Pods_Calculator_Quick
-@end
